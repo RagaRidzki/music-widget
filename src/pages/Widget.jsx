6270a9dcb0921e7ace7d0f4f673d7d44466c17d5
RagaRidzki <ragaridzkyy@gmail.com>
2025-11-08 11:58:45 +0700

    chore: scaffold react + tailwind + routing setup

---
+++ src/pages/Widget.jsx
@@ -0,0 +1,8 @@
+export default function Widget() {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-gray-800 text-white">
+        <h1 className="text-3xl font-bold">Widget Player Page</h1>
+      </div>
+    );
+  }
+  
