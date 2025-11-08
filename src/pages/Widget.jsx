f031ba8a6ed61c0572e84da02acc6d282a8db7fc
RagaRidzki <ragaridzkyy@gmail.com>
2025-11-09 00:00:30 +0700

    feat(ui): design ui widget v1.5.3

--- src/pages/Widget.jsx
+++ src/pages/Widget.jsx
@@ -133,7 +133,7 @@ export default function Widget() {
 
   return (
     <div className="fixed bottom-4 right-4 z-50">
-      <div className="relative w-[232px]">
+      <div className="relative w-40">
         {" "}
         {showList && (
           <div
